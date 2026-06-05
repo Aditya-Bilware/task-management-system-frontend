9ad6bb318f3e39058ffbcb1dffcdfa7638ac4952
Aditya Bilware <aditya.bilware@necsws.com>
2026-06-05 17:56:11 +0530

    enable render server

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://task-management-system-8dvg.onrender.com/api",
-  // baseURL: import.meta.env.VITE_API_URL,
   // baseURL: "https://task-management-system-8dvg.onrender.com/api",
+  // baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://task-management-system-8dvg.onrender.com/api",
 });
 
 API.interceptors.request.use(
