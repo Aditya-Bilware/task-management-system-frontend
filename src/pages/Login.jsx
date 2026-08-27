71b3d03cfc64ce7311e1dee59b20cf6d9909c2a5
Aditya Bilware <aditya.bilware@necsws.com>
2026-08-27 15:28:58 +0530

    restrict api call if password length is less than 6

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -36,7 +36,7 @@ const LoginPage = () => {
     try {
       dispatch(setLoading(true));
 
-      if (password.length < 5) return;
+      if (password.length < 6) return;
 
       const data = await loginUser({
         email,
@@ -270,7 +270,7 @@ const LoginPage = () => {
             fullWidth
             variant="contained"
             type="submit"
-            disabled={loading || password.length < 5}
+            disabled={loading || password.length < 6}
             sx={{
               py: 1.3,
               borderRadius: "10px",
